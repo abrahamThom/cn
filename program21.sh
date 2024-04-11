d8c43d58fc81b496a74df509b477d49aff9d163b
prins_____18 <100407578+abrahamThom@users.noreply.github.com>
2024-04-11 11:59:22 +0530

    Add files via upload

---
+++ program21.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author name: Prince Thomas
+#23mca045
+echo "Enter the number of terms:"
+read n
+a=0
+b=1
+echo "Fibonacci Series:"
+for ((i=0; i<n; i++)); do
+    echo "$a "
+    fn=$((a + b))
+    a=$b
+    b=$fn
+done
